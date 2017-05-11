0f52dca7fc595bfe24ed3643c7cb16beb221915f
Linquas <steven84059@gmail.com>
2017-05-11 14:20:50 +0800

    add detail Pokemon View

--- pokedex/PokemonDeyailVC.swift
+++ pokedex/PokemonDeyailVC.swift
@@ -11,6 +11,17 @@ import UIKit
 class PokemonDeyailVC: UIViewController {
 
     
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var ackLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var defLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     var pokemon: Pokemon!
     
@@ -21,6 +32,9 @@ class PokemonDeyailVC: UIViewController {
     }
 
    
+    @IBAction func backBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
 
     
